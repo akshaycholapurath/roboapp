f8b29f6ae5d1490faed2f7e29bb8d091b7be329d
root <root@Ghost.localdomain>
2021-02-27 12:44:52 +0530

    minor update

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = ({children})=>{
     return (
-        <div style={{overflowY:'scroll',border:'3px solid black',height:'500px'}}>
+        <div style={{overflowY:'scroll',border:'3px solid black',height:'800px'}}>
             {children}
         </div>
     )
